fe42f7cd234b94dc375060c41950a4dedc00c799
ryu-sugi <ryuu085@gmail.com>
2022-04-05 14:54:52 +0900

    Vue.componentの基本構造

--- show-hello.js
+++ show-hello.js
@@ -1,1 +1,26 @@
-Vue.component('show-hello',{template: '<p>Hello Vue!</p>'});
+// コンポーネント
+Vue.component('show-hello',{
+  // テンプレート部分
+  template: '<p>{{message}}</p>',
+  // dataオプション
+  data: function() {
+    return {
+      message: 'Hello Vue'
+    }
+  },
+  // メソッド
+  methods: {
+  },
+  // 算出プロパティ
+  computed: {
+  },
+  // ウォッチャ
+  watch: {
+  },
+  // フィルター
+  filters: {
+  },
+  // ライフサイクルハック
+  created: function() {
+  }
+});
